6e0c6b598533d90b941c725682c9e64b75a612a0
Julian Pachon <pachonandres875@gmail.com>
2025-08-13 16:09:51 -0500

    juego-funcional

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+function agregarAmigo(){
+    let Amigos = document.getElementById('amigo').value.trim();
+
+    if (Amigos) {
+        listaAmigos.push(Amigos);
+        mostrarLista();
+        document.getElementById("amigo").value = "";
+    } else {
+        alert("Ingrese un nombre valido");
+    }
+}
+
+function mostrarLista() {
+    const listaHTML = document.getElementById("listaAmigos");
+    listaHTML.innerHTML = ""; 
+
+    listaAmigos.forEach(amigo => {
+    const li = document.createElement("li");
+    li.textContent = amigo;
+    listaHTML.appendChild(li);
+    });
+}
+
+function sortearAmigo(){
+    let seleccion = Math.floor(Math.random() * listaAmigos.length);
+
+    let listaResultado = document.getElementById('resultado');
+
+    let li = document.createElement('li');
+    li.textContent = `El amigo secreto es: ${listaAmigos[seleccion]}`;
+    
+    limpiarLista();
+    listaResultado.appendChild(li);
+}
+
+function limpiarLista() {
+    let limpiarListaAmigos = document.getElementById('listaAmigos');
+    limpiarListaAmigos.innerHTML = "";
+}
+
